90c40c8ab4d2bb98137e1544f121ac6304e70111
MunSeungJun <sbs2469@naver.com>
2025-05-13 17:58:14 +0900

    feat: add LoginPage route as child of Mainlayout

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Mainlayout from '../layouts/Mainlayout'
+import LoginPage from '../pages/login/LoginPage'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Mainlayout />}></Route>
+      <Route path="/" element={<Mainlayout />}>
+        <Route path="login" element={<LoginPage />} />
+      </Route>
     </Routes>
   )
 }
