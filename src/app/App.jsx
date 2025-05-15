1e23f64a59eec749545b18699f7bec3a2582732e
STMT <elahep113@naver.com>
2025-05-15 10:22:48 +0900

    chore: List, ViewPlace 추가, 1. List는 페이지만 개설 2. ViewPlace (작업 진행중)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Mainlayout from '../layouts/Mainlayout.jsx'
 import HomeSearch from '../features/home/HomeSearch.jsx'
+import List from '../features/List.jsx'
+import ViewPlace from '../features/ViewPlace.jsx'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Mainlayout />}>
         <Route index element={<HomeSearch />}></Route>
+        <Route path='list' element={<List />}></Route>
+        <Route path='place/:id' element={<ViewPlace />}></Route>
       </Route>
     </Routes>
   )
