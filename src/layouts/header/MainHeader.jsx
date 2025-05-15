ddfae3252d8bf902cf18586fd6acda5e53747d06
STMT <elahep113@naver.com>
2025-05-15 10:26:38 +0900

    feat: header 구조 개선 + ('/') 루트 페이지 기준 화면 ui 변경

--- src/layouts/header/MainHeader.jsx
+++ src/layouts/header/MainHeader.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
-import { useNavigate, Link } from 'react-router'
+import { useNavigate, useLocation } from 'react-router-dom'
 import Logo from '../../assets/images/logo.png'
+import LogoSm from '../../assets/images/logo-small.png'
 import GoogleMaps from '../../features/googleMapsApi/GoogleMaps'
+import usePlaceStore from '../../store/usePlaceStore'
 
 const MainHeader = () => {
   const navigate = useNavigate()
-
+  const location = useLocation()
+  const isRoot = location.pathname === '/'
+  // const isList = location.pathname.startsWith('/list')
+  const setPlaceDetails = usePlaceStore(state => state.setPlaceDetails)
   const navigateLogin = () => {
     // navigate('/login')
     console.log('로그인 이동')
@@ -15,37 +20,64 @@ const MainHeader = () => {
     console.log('회원가입 이동')
   }
   const navigateHome = () => {
+    setPlaceDetails(null)
     navigate('/')
   }
   return (
     <>
       <div className="w-full pb-5">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex justify-between items-center">
-            <div onClick={navigateHome}>
-              <img src={Logo} alt="logo" className="mouse_pointer" />
+        {isRoot === true ? (
+          <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between items-center">
+              <div onClick={navigateHome}>
+                <img src={Logo} alt="logo" className="mouse_pointer" />
+              </div>
+              <div className="flex gap-3">
+                <button
+                  type="button"
+                  className="border bg-teal-400 text-white border-black rounded-3xl p-2 font-sans mouse_pointer"
+                  onClick={navigateLogin}
+                >
+                  로그인
+                </button>
+                <button
+                  type="button"
+                  className="border bg-white text-teal-400 border-black rounded-3xl p-2 font-sans mouse_pointer"
+                  onClick={navigateSignup}
+                >
+                  회원가입
+                </button>
+              </div>
             </div>
-            <div className="flex gap-3">
-              <button
-                type="button"
-                className="border bg-teal-400 text-white border-black rounded-3xl p-2 font-sans mouse_pointer"
-                onClick={navigateLogin}
-              >
-                로그인
-              </button>
-              <button
-                type="button"
-                className="border bg-white text-teal-400 border-black rounded-3xl p-2 font-sans mouse_pointer"
-                onClick={navigateSignup}
-              >
-                회원가입
-              </button>
-            </div>
+            <h1 className="text-center text-5xl font-sans font-bold">어디로 가시나요?</h1>
+            <GoogleMaps />
           </div>
-          <h1 className="text-center text-5xl font-sans font-bold">어디로 가시나요?</h1>
-          <nav></nav>
-        </div>
-        <GoogleMaps />
+        ) : (
+          <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between items-center">
+              <div onClick={navigateHome}>
+                <img src={LogoSm} alt="logo" className="mouse_pointer" />
+              </div>
+              <div className="flex gap-3">
+                <button
+                  type="button"
+                  className="border bg-teal-400 text-white border-black rounded-3xl p-2 font-sans mouse_pointer"
+                  onClick={navigateLogin}
+                >
+                  로그인
+                </button>
+                <button
+                  type="button"
+                  className="border bg-white text-teal-400 border-black rounded-3xl p-2 font-sans mouse_pointer"
+                  onClick={navigateSignup}
+                >
+                  회원가입
+                </button>
+              </div>
+            </div>
+            <GoogleMaps />
+          </div>
+        )}
       </div>
     </>
   )
