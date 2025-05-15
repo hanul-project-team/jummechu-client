fcef65db62164ae4b827b8e7c3a44d66d2bbf821
MunSeungJun <sbs2469@naver.com>
2025-05-15 14:24:59 +0900

    feat: 로그인 상태 + 유저정보 전역 상태 변수 임시 추가

--- src/layouts/header/MainHeader.jsx
+++ src/layouts/header/MainHeader.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const MainHeader = () => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+  const user = useSelector(state => state.auth.user)
+  console.log(isAuthenticated)
+  console.log(user)
   return (
     <div>MainHeader</div>
   )
