e8f6ba25f6379fd5c3ae431011520611237d91fe
MunSeungJun <sbs2469@naver.com>
2025-05-15 15:55:06 +0900

    feat: redux store 생성 및 authReducer 등록 + provider에 store 전달

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import reduxStore from './reduxStore'
 import App from './App'
 import '../assets/styles/global.css'
 import '../assets/styles/tailwind.css'
@@ -8,8 +10,10 @@ import '../assets/styles/tailwind.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <Provider store={reduxStore}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </Provider>
   </BrowserRouter>,
 )
