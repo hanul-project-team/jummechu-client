16692f409a916cfbed2caf82fb351bcbfe3d00a9
STMT <elahep113@naver.com>
2025-05-16 18:14:38 +0900

    chore: 검색어 결과 페이지 준비

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Mainlayout from '../layouts/Mainlayout.jsx'
 import HomeSearch from '../features/home/HomeSearch.jsx'
 import List from '../features/List.jsx'
-import ViewPlace from '../features/ViewPlace.jsx'
+// import ViewPlace from '../features/ViewPlace.jsx'
+import ViewResult from '../features/ViewResult.jsx'
 import { useDispatch } from 'react-redux'
 import { authCheck } from '../features/auth/authSlice'
 import LoginPage from '../pages/auth/LoginPage'
@@ -18,7 +19,8 @@ const App = () => {
       <Route path="/" element={<Mainlayout />}>
         <Route index element={<HomeSearch />}></Route>
         <Route path='list' element={<List />}></Route>
-        <Route path='place/:id' element={<ViewPlace />}></Route>
+        {/* <Route path='place/:id' element={<ViewPlace />}></Route> */}
+        <Route path='search/:query' element={<ViewResult />}></Route>
         <Route path="login" element={<LoginPage />} />
       </Route>
     </Routes>
