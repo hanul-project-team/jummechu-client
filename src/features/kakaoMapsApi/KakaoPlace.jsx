fad07502968e8ccf3f8dab0906a1d3a5dd3d64dd
STMT <elahep113@naver.com>
2025-05-16 18:19:43 +0900

    chore: swiper mapping 구조 변경

--- src/features/kakaoMapsApi/KakaoPlace.jsx
+++ src/features/kakaoMapsApi/KakaoPlace.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import usePlaceStore from '../../store/usePlaceStore.js'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const KakaoPlace = () => {
-  // const kakaoPlace = usePlaceStore(state => state.kakaoPlace)
-  //   console.log(kakaoPlace)
-  //   console.log(placeDetails)
+  const kakaoPlace = usePlaceStore(state => state.kakaoPlace)
+  // console.log(kakaoPlace)
   return (
     <>
-      <h3 className="text-2xl">관련 장소 &#40;아닐 수 있음&#41;</h3>
-      <div className="container w-full">
-        {/* {kakaoPlace !== null && kakaoPlace !== '' && (
-          <div>
-            {kakaoPlace.map((kp, i) => (
-              <div key={i} className="flex gap-2">
-                <div>
-                  <img src='https://picsum.photos/150/150' />
-                </div>
-                <div className='font-sans md:font-mono sm:font-serif'>
-                  <p><strong>상호명</strong>:{kp.place_name}</p>
-                  <p><strong>소재지</strong>:{kp.address_name}</p>
-                </div>
-              </div>
+      <div>
+        <h3 className="text-2xl">가까운 장소</h3>
+      </div>
+      <div className="container w-full py-2 px-5 shadow-xl/20">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          // onSlideChange={() => console.log('slide change')}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          {kakaoPlace &&
+            kakaoPlace.map((kp, i) => (
+              <SwiperSlide key={i} className='md:max-w-full' style={{margin: '0px'}}>
+                <img src="https://picsum.photos/200/150" alt="lorem picture" />
+                <p className='font-bold font-sans max-[426px]:text-sm'>{kp.place_name}</p>
+                <p className='max-[426px]:text-sm'>소재지:{kp.address_name}</p>
+              </SwiperSlide>
             ))}
-          </div>
-        )} */}
+        </Swiper>
       </div>
     </>
   )
