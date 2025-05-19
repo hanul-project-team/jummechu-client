60ea0a214d9f5e18784fde60ea6269b39af2b600
MunSeungJun <sbs2469@naver.com>
2025-05-19 18:12:03 +0900

    feat: 회원가입 폼 기능 구현 임시 저장
    -----     완료       ------
    유효성 검증 및 포커스 설정
    비밀번호 가시성
    단계별 ui 렌더링 및 버튼 제어
    ----- 추가할 기능 -----
    sms인증 서버 요청
    비밀번호 힌트 확인

--- src/features/auth/components/regist/RegistDetailsForm.jsx
+++ src/features/auth/components/regist/RegistDetailsForm.jsx
@@ -1,37 +1,279 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import { registScheam } from '../../schema/registSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import VisibleBtn from '../../../../shared/VisibleBtn'
+import Timer from '../../../../shared/Timer'
 import style from './registDetailsForm.module.css'
 
 const RegistDetailsForm = () => {
-    const { register, handleSubmit, formState: {errors}} = useForm()
+  const [isOpen, setIsOpen] = useState(false)
+  const [isPhone, setIsPhone] = useState(false)
+  const [isRequested, setIsRequested] = useState(false)
+  const [isCode, setIsCode] = useState(false)
+  const [isSMSAuthenticated, setIsSMSAuthenticated] = useState(false)
+  const [timerKey, setTimerKey] = useState(0)
+  const [passwordState, setPasswordState] = useState({
+    hasValue: false,
+    visible: false,
+  })
+  const [passwordCheckState, setPasswordCheckState] = useState({
+    hasValue: false,
+    visible: false,
+  })
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    watch,
+    trigger,
+    getValues,
+    resetField,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registScheam),
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+  })
+  const navigate = useNavigate()
+  const onExpire = () => {
+    setIsRequested(false)
+  }
+  const phoneSubmit = async () => {
+    const isValid = await trigger('phone')
+    if (isValid) {
+      const phone = getValues('phone')
+      console.log(phone)
+      // await axios.post('http://localhost:3000/auth/reqcode', phone)
+      setIsRequested(true)
+      resetField('code')
+      setFocus('code')
+      setTimerKey(prev => prev + 1)
+    }
+  }
+  const codeSubmit = async () => {
+    const isValid = await trigger('code')
+    if (isValid) {
+      const code = getValues('code')
+      console.log(code)
+      // try {
+      //   const response = await axios.post('http://localhost:3000/auth/reqauthenticate', code)
+      //   if (response.data === true) {
+      //   }
+      // } catch {}
+      setIsSMSAuthenticated(true)
+    }
+  }
+  const onSubmit = async data => {
+    const role = JSON.parse(localStorage.getItem('role'))
+    const termsAgreement = JSON.parse(localStorage.getItem('termsAgreement'))
+    const { passwordCheck, code, ...rest } = data
+    const submitData = { role, termsAgreement, rest }
+    console.log(submitData)
+    navigate('/login')
+    // try {
+    //   await axios.post('http://localhost:3000/auth/regist', submitData)
+    // } catch {}
+  }
+  const onIsvalid = errors => {
+    if (errors.email) {
+      setFocus('email')
+    } else if (errors.password) {
+      setFocus('password')
+    } else if (errors.passwordCheck) {
+      setFocus('passwordCheck')
+    } else if (errors.name) {
+      setFocus('name')
+    } else if (errors.phone) {
+      setFocus('phone')
+    }
+  }
+  const phoneValue = watch('phone')
+  useEffect(() => {
+    const isValid = /^01[016789][0-9]{8}$/.test(phoneValue)
+    setIsPhone(!!isValid)
+  }, [phoneValue])
+  const codeValue = watch('code')
+  useEffect(() => {
+    const isValid = /^\d{6}$/.test(codeValue)
+    setIsCode(!!isValid)
+  }, [codeValue])
+  const passwordValue = watch('password')
+  const passwordCheckValue = watch('passwordCheck')
+  useEffect(() => {
+    setPasswordState(prev => ({
+      ...prev,
+      hasValue: !!passwordValue,
+    }))
+  }, [passwordValue])
+  useEffect(() => {
+    setPasswordCheckState(prev => ({
+      ...prev,
+      hasValue: !!passwordCheckValue,
+    }))
+  }, [passwordCheckValue])
+
+  const changeVisible = setter => {
+    setter(prev => ({
+      ...prev,
+      visible: !prev.visible,
+    }))
+  }
   return (
-    <form className={style.loginForm} autoComplete='off'>
-        <fieldset className="flex flex-col gap-3">
-            <legend className='hidden'>상세정보 입력 폼</legend>
-            <div className={`${style.loginFormField} flex flex-col`}>
-                <label htmlFor="email">이메일<span className={style.requiredSpan}>*</span></label>
-                <input className="grow" type="text" id='email' placeholder='이메일' {...register('email')} />
+    <form
+      className={style.registForm}
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit, onIsvalid)}
+    >
+      <fieldset className="flex flex-col gap-3">
+        <legend className="hidden">상세정보 입력 폼</legend>
+        <div className={`${style.registFormField} flex flex-col`}>
+          <label htmlFor="email">
+            이메일<span className={style.requiredSpan}>*</span>
+          </label>
+          <input
+            className="grow"
+            type="text"
+            id="email"
+            placeholder="이메일"
+            {...register('email')}
+          />
+          {errors.email && <span className={style.errorSpan}>{errors.email.message}</span>}
+        </div>
+        <div className={`${style.registFormField} flex flex-col`}>
+          <div className='flex gap-3'>
+            <label htmlFor="password">
+              비밀번호<span className={style.requiredSpan}>*</span>
+            </label>
+            <button onClick={() => setIsOpen(true)}>열기</button>
+          </div>
+          <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+            <div className={`${style.modalWrapper} fixed flex`}>
+              <DialogPanel className={`${style.modalPanel}`}>
+                <p>비밀번호는 다음 조건을 만족해야 합니다:</p>
+                <ul>
+                  <li>8자 이상, 영문, 숫자, 특수문자 포함</li>
+                  <li>~ ! @ # ^ * _ = + - </li>
+                </ul>
+              </DialogPanel>
             </div>
-            <div className={`${style.loginFormField} flex flex-col`}>
-                <label htmlFor="password">비밀번호<span className={style.requiredSpan}>*</span></label>
-                <input className="grow" type="password" id='password' placeholder='비밀번호' {...register('password')} />
+          </Dialog>
+          <input
+            className="grow"
+            type={passwordState.visible ? 'text' : 'password'}
+            id="password"
+            placeholder="비밀번호"
+            {...register('password')}
+          />
+          <VisibleBtn
+            changeVisible={changeVisible}
+            setter={setPasswordState}
+            visible={passwordState.visible}
+            hasValue={passwordState.hasValue}
+            className="absolute top-11 right-3 "
+          />
+          {errors.password && <span className={style.errorSpan}>{errors.password.message}</span>}
+        </div>
+        <div className={`${style.registFormField} flex flex-col`}>
+          <input
+            className="grow"
+            type={passwordCheckState.visible ? 'text' : 'password'}
+            id="passwordCheck"
+            placeholder="비밀번호 확인"
+            {...register('passwordCheck')}
+          />
+          <VisibleBtn
+            changeVisible={changeVisible}
+            setter={setPasswordCheckState}
+            visible={passwordCheckState.visible}
+            hasValue={passwordCheckState.hasValue}
+            className="absolute top-5 right-3 "
+          />
+          {errors.passwordCheck && (
+            <span className={style.errorSpan}>{errors.passwordCheck.message}</span>
+          )}
+        </div>
+        <div className={`${style.registFormField} flex flex-col`}>
+          <label htmlFor="name">
+            이름<span className={style.requiredSpan}>*</span>
+          </label>
+          <input className="grow" type="text" id="name" placeholder="이름" {...register('name')} />
+          {errors.name && <span className={style.errorSpan}>{errors.name.message}</span>}
+        </div>
+        <div className={`${style.registFormField} flex flex-col`}>
+          <label htmlFor="phone">
+            전화번호<span className={style.requiredSpan}>*</span>
+          </label>
+          <div className="flex gap-3">
+            <input
+              className={`${style.noSpinner} grow`}
+              type="text"
+              id="phone"
+              placeholder="'-'제외 숫자만 입력해주세요"
+              maxLength={11}
+              {...register('phone')}
+              onInput={e => {
+                e.target.value = e.target.value.replace(/[^0-9]/g, '').slice(0, 11)
+              }}
+            />
+            {!isRequested && !isSMSAuthenticated && (
+              <button
+                type="button"
+                onClick={phoneSubmit}
+                disabled={!isPhone}
+                className={style.getCodeBtn}
+              >
+                인증하기
+              </button>
+            )}
+            {isRequested && !isSMSAuthenticated && (
+              <button
+                type="button"
+                onClick={phoneSubmit}
+                disabled={!isPhone}
+                className={style.resendBtn}
+              >
+                재전송하기
+              </button>
+            )}
+          </div>
+          {errors.phone && <span className={style.errorSpan}>{errors.phone.message}</span>}
+        </div>
+        {isRequested && !isSMSAuthenticated && (
+          <div className={`${style.registFormField} flex flex-col`}>
+            <label htmlFor="phone">
+              <Timer key={timerKey} duration={180} onExpire={onExpire} />
+            </label>
+            <div className="flex gap-3">
+              <input
+                className={`${style.noSpinner} grow`}
+                type="text"
+                id="code"
+                placeholder="인증번호 6자리"
+                maxLength={6}
+                {...register('code')}
+                onInput={e => {
+                  e.target.value = e.target.value.replace(/[^0-9]/g, '').slice(0, 6)
+                }}
+              />
+              <button
+                type="button"
+                onClick={codeSubmit}
+                disabled={!isCode}
+                className={style.sendCodeBtn}
+              >
+                확인
+              </button>
             </div>
-            <div className={`${style.loginFormField} flex flex-col`}>
-                <input className="grow" type="password" id='passwordCheck' placeholder='비밀번호 확인' {...register('passwordCheck')} />
-            </div>
-            <div className={`${style.loginFormField} flex flex-col`}>
-                <label htmlFor="name">이름<span className={style.requiredSpan}>*</span></label>
-                <input className="grow" type="text" id='name' placeholder='이름' {...register('name')} />
-            </div>
-            <div className={`${style.loginFormField} flex flex-col`}>
-                <label htmlFor="phone">전화번호<span className={style.requiredSpan}>*</span></label>
-                <input className={`${style.noSpinner} grow`} type="number" id='phone' placeholder='전화번호' {...register('phone')} />
-            </div>
-            <div className={`${style.loginFormField} flex flex-col`}>
-                <label htmlFor="phone">남은시간 알려주기</label>
-                <input className={`${style.noSpinner} grow`} type="number" id='phone' placeholder='인증번호 6자리' {...register('phone')} />
-            </div>
-        </fieldset>
+          </div>
+        )}
+        <button type="submit" disabled={!isSMSAuthenticated} className={style.registBtn}>
+          가입하기
+        </button>
+      </fieldset>
     </form>
   )
 }
