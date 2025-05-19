e58bced78317ac67725bd022c064ed3966bd25ec
STMT <elahep113@naver.com>
2025-05-19 13:05:42 +0900

    chore: 필요없는 jsx 주석

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import Mainlayout from '../layouts/Mainlayout.jsx'
 import HomeSearch from '../features/home/HomeSearch.jsx'
 import List from '../features/List.jsx'
-// import ViewPlace from '../features/ViewPlace.jsx'
 import ViewResult from '../features/ViewResult.jsx'
 import { useDispatch } from 'react-redux'
 import { authCheck } from '../features/auth/authSlice'
 import LoginPage from '../pages/auth/LoginPage'
+// import ViewPlace from '../features/ViewPlace.jsx'
 import MyPages from '../pages/mypage/MyPage.jsx'
 
 const App = () => {
@@ -21,11 +21,10 @@ const App = () => {
         <Route path="/mypage" element={<MyPages />}></Route>
         <Route index element={<HomeSearch />}></Route>
         <Route path='list' element={<List />}></Route>
-        {/* <Route path='place/:id' element={<ViewPlace />}></Route> */}
         <Route path='search/:query' element={<ViewResult />}></Route>
         <Route path="login" element={<LoginPage />} />
         <Route path="list" element={<List />}></Route>
-        <Route path="place/:id" element={<ViewPlace />}></Route>
+        {/* <Route path="place/:id" element={<ViewPlace />}></Route> */}
       </Route>
     </Routes>
   )
