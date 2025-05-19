146ea3ffc200513e64c00dc68632c5642857d9df
STMT <elahep113@naver.com>
2025-05-19 16:57:00 +0900

    chore: 사용하지 않는 전역 변수 삭제

--- src/store/usePlaceStore.js
+++ src/store/usePlaceStore.js
@@ -2,12 +2,10 @@ import {create} from 'zustand';
 
 const usePlaceStore = create((set) => ({
     searchData: null,
-    nearPlaces: [],
     center: null,
     kakaoPlace: null,
 
     setSearchData: data => set({searchData: data}),
-    setNearPlaces: data => set({nearPlaces: data}),
     setCenter: data => set({center: data}),
     setKakaoPlace: data => set({kakaoPlace: data}),
 }))
