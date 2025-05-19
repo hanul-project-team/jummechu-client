c1b471ac859823a040d1d2a753b2e5d71daa4649
MunSeungJun <sbs2469@naver.com>
2025-05-19 17:18:29 +0900

    refactor: 비밀번호 가시성 버튼 추가에 따른 코드 수정

--- src/features/auth/components/login/LoginForm.jsx
+++ src/features/auth/components/login/LoginForm.jsx
@@ -7,12 +7,15 @@ import axios from 'axios'
 import { Field, Label } from '@headlessui/react'
 import { loginSchema } from '../../schema/loginSchema'
 import { login } from '../../authSlice'
+import VisibleBtn from '../../../../shared/VisibleBtn'
 import CustomCheckBox from '../../../../shared/CustomCheckBox'
 import style from './loginForm.module.css'
 
 const LoginForm = () => {
-  const [visible, setVisible] = useState(false)
-  const [isPassword, setIsPassword] = useState(false)
+  const [passwordState, setPasswordState] = useState({
+      hasValue: false,
+      visible: false,
+    })
   const [serverError, setServerError] = useState()
   const {
     register,
@@ -32,13 +35,12 @@ const LoginForm = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const passwordValue = watch('password')
-  useEffect(() => {
-    if (passwordValue) {
-      setIsPassword(true)
-    } else {
-      setIsPassword(false)
-    }
-  }, [passwordValue])
+   useEffect(() => {
+      setPasswordState(prev => ({
+        ...prev,
+        hasValue: !!passwordValue,
+      }))
+    }, [passwordValue])
   const onSubmit = async data => {
     try {
       const response = await axios.post('http://localhost:3000/auth/login', data, {
@@ -66,12 +68,11 @@ const LoginForm = () => {
     }
     resetField('password', { keepError: true })
   }
-  const changeVisible = () => {
-    if (visible === true) {
-      setVisible(false)
-    } else {
-      setVisible(true)
-    }
+  const changeVisible = setter => {
+    setter(prev => ({
+      ...prev,
+      visible: !prev.visible,
+    }))
   }
   return (
     <form
@@ -89,50 +90,17 @@ const LoginForm = () => {
         <div className={`${style.loginFormField} flex flex-col`}>
           <input
             className="grow"
-            type={visible ? 'text' : 'password'}
+            type={passwordState.visible ? 'text' : 'password'}
             id="password"
             placeholder=""
             {...register('password')}
           />
           <label htmlFor="password">비밀번호</label>
-          <button type="button" onClick={changeVisible} className={isPassword ? 'flex' : 'hidden'}>
-            {visible ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                />
-              </svg>
-            )}
-          </button>
+          <VisibleBtn changeVisible={changeVisible}
+            setter={setPasswordState}
+            visible={passwordState.visible}
+            hasValue={passwordState.hasValue}
+            className="absolute top-5 right-3 "  />
         </div>
         <Controller
           name="rememberMe"
