f0aa614f605bf9ba9fd4eac0bb4de93e3a26aee1
STMT <elahep113@naver.com>
2025-05-19 18:01:36 +0900

    chore: 행동에 맞게 명칭 변경 viewresult에서 searchresult로

---
+++ src/features/SearchResult.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import usePlaceStore from '../store/usePlaceStore'
+import { Link } from 'react-router-dom'
+
+const SearchResult = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const searchData = usePlaceStore(state => state.searchData)
+
+  useEffect(() => {
+    if (searchData && searchData.length > 0) {
+      setIsLoading(false)
+    }
+  }, [searchData])
+  // console.log(isLoading)
+  // console.log(searchData)
+  return (
+    <div className="container max-w-3/5 mx-auto">
+      {isLoading === true ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          <p>{searchData.length} 개의 검색 결과</p>
+          {searchData.map((sd, i) => {
+            return (
+              <div key={i} className="flex gap-2 p-2 my-3 border-1 rounded-xl">
+                <div className="md:min-w-[200px]">
+                  <Link to={`/place/${sd.id}`}>
+                    <img
+                      src={`https://picsum.photos/200/200?random=${Math.floor(Math.random() * 1000)}`}
+                      alt="picsum"
+                      className="md:w-[200px] md:h-[200px] sm:w-[150px] sm:h-[150px] object-cover rounded-lg"
+                    />
+                  </Link>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="text-2xl">
+                    <Link to={`/place/${sd.id}`}>
+                      <strong>
+                        {i + 1}. {sd.place_name}
+                      </strong>
+                    </Link>
+                  </span>
+                  <p>
+                    <strong>주소지</strong>:{sd.address_name}
+                  </p>
+                  <p>
+                    {sd.phone ? (
+                      <>
+                        <strong>연락처: </strong>
+                        <span>{sd.phone}</span>
+                      </>
+                    ) : (
+                      '연락처 미공개'
+                    )}
+                  </p>
+                  <div>
+                    <div className="flex gap-2">
+                      <p>
+                        <strong>태그: </strong>
+                        {sd.summary.keyword}
+                      </p>
+                      {/* {sd.summary.keyword.split(',').map((kw, i) => (
+                        <p key={i}>{kw}</p>
+                      ))} */}
+                    </div>
+                    <p><strong>개요</strong>: {sd.summary.description}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </>
+      )}
+    </div>
+  )
+}
+
+export default SearchResult
--- src/features/ViewResult.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ViewResult = () => {
-  return (
-    <div>ViewResult</div>
-  )
-}
-
-export default ViewResult
