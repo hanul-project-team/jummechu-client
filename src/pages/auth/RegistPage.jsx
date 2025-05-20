5cf8f3e4b56c0da0e9245eedcdada701f5bda568
MunSeungJun <sbs2469@naver.com>
2025-05-20 17:29:53 +0900

    feat: content title 추가 및 content wrapper max-width 수정

--- src/pages/auth/RegistPage.jsx
+++ src/pages/auth/RegistPage.jsx
@@ -3,7 +3,8 @@ import { useNavigate, useParams } from 'react-router-dom'
 import RegistTypeForm from '../../features/auth/components/regist/RegistTypeForm'
 import TermsForm from '../../features/auth/components/regist/TermsForm'
 import RegistDetailsForm from '../../features/auth/components/regist/RegistDetailsForm'
-import style from './registPage.module.css'
+import RegistAction from '../../features/auth/components/regist/RegistAction'
+
 
 const RegistPage = () => {
   const navigate = useNavigate()
@@ -15,21 +16,23 @@ const RegistPage = () => {
 
   return (
     <main className="container mx-auto flex justify-center ">
-      <section className={`${style.contentWrapper} flex flex-col`}>
-        <div className={`flex justify-center pb-3`}>회원가입</div>
-
+      <section className={`${!(step === 'type') && 'max-w-96'} w-full flex flex-col`}>
         {step === 'type' && (
-          <div className={`${style.contentBody} flex flex-col`}>
+          <div className="flex flex-col gap-3">
+            <h2 className='text-center text-2xl mb-10'>맛집을 찾으시나요, 운영하시나요? <br />지금 선택해보세요</h2>
             <RegistTypeForm nextStep={nextStep} />
+            <RegistAction />
           </div>
         )}
         {step === 'terms' && (
-          <div className={`${style.contentBody} flex flex-col`}>
+          <div className="flex flex-col gap-10">
+            <h2 className='text-center text-2xl'>서비스 이용을 위해 <br /> 약관에 동의해주세요</h2>
             <TermsForm nextStep={nextStep} />
           </div>
         )}
         {step === 'details' && (
-          <div className={`${style.contentBody} flex flex-col`}>
+          <div className="flex flex-col gap-10">
+            <h2 className='text-center text-2xl'>회원 정보를 입력해주세요</h2>
             <RegistDetailsForm />
           </div>
         )}
