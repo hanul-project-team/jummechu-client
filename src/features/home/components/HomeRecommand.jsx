18b71187edbfbf04fdfa8cfd2bb0080eab5913ba
STMT <elahep113@naver.com>
2025-05-20 17:42:01 +0900

    style: 코드 구조 변경 1. 기존 - 직접 배열의 idx를 넣어 그에 맞는 태그의 정보를 불러옴 2. 변경 - 태그 자체를 반복화해서 반복되는 태그에 맞는 배열 정보를 다시 반복문으로 불러오게 변경

--- src/features/home/components/HomeRecommand.jsx
+++ src/features/home/components/HomeRecommand.jsx
@@ -5,9 +5,10 @@ import 'swiper/css'
 import { Link } from 'react-router-dom'
 import usePlaceStore from '../../../store/usePlaceStore.js'
 import '../../../assets/styles/tailwind.css'
+import '../../../assets/styles/global.css'
 
 const HomeRecommand = () => {
-  const [tag, setTag] = useState(null)
+  const [tag, setTag] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   const kakaoPlace = usePlaceStore(state => state.kakaoPlace)
@@ -33,13 +34,42 @@ const HomeRecommand = () => {
   return (
     <div className="max-xl:m-3">
       {isLoading === true ? (
-        <p>loading...</p>
+        <p className="loading-jump">Loading<span className="jump-dots"><span>.</span><span>.</span><span>.</span></span></p>
       ) : (
         <>
           <Link to="/list">
             <span className="text-xl font-bold font-(family-name:peoplefirst)">전체보기</span>
           </Link>
-          <div className="container shadow-xl/20 max-w-full p-3 my-3 overflow-auto">
+          {tag.map((t, i) => (
+            <div key={i} className="container shadow-xl/20 max-w-full p-3 my-3 overflow-auto">
+              <p className="text-lg font-sans">&#35; {t}</p>
+              <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                // onSlideChange={() => console.log('slide change')}
+                // onSwiper={swiper => console.log(swiper)}
+              >
+                {kakaoPlace &&
+                  kakaoPlace.length > 0 &&
+                  kakaoPlace.map((kp, idx) => {
+                    if (!kp.category_name.includes(t)) return null
+                    return (
+                      <SwiperSlide key={idx}>
+                        <div className="min-w-50">
+                          <img src={Icon} alt="picsum" style={{ minHeight: '200px' }} />
+                          <p className="text-sm">가게명:{kp.place_name}</p>
+                          <div className="text-sm flex gap-3 items-center">
+                            <span>4.3☆</span>
+                            <span>&#40;{`리뷰수`}&#41;</span>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                    )
+                  })}
+              </Swiper>
+            </div>
+          ))}
+          {/* <div className="container shadow-xl/20 max-w-full p-3 my-3 overflow-auto">
             <p className="text-lg font-sans">&#35; {tag[0]}</p>
             <Swiper
               spaceBetween={50}
@@ -65,61 +95,8 @@ const HomeRecommand = () => {
                   )
                 })}
             </Swiper>
-          </div>
-          <div className="container shadow-xl/20 max-w-full p-3 my-3 overflow-auto">
-            <p className="text-lg font-sans">&#35; {tag[1]}</p>
-            <Swiper
-              spaceBetween={50}
-              slidesPerView={3}
-              // onSlideChange={() => console.log('slide change')}
-              // onSwiper={swiper => console.log(swiper)}
-            >
-              {kakaoPlace &&
-                kakaoPlace.length > 0 &&
-                kakaoPlace.map((kp, i) => {
-                  if (!kp.category_name.includes(tag[1])) return null
-                  return (
-                    <SwiperSlide key={i}>
-                      <div className="min-w-50">
-                        <img src={Icon} alt="picsum" style={{ minHeight: '200px' }} />
-                        <p className="text-sm">가게명:{kp.place_name}</p>
-                        <div className="text-sm flex gap-3 items-center">
-                          <span>4.3☆</span>
-                          <span>&#40;{`리뷰수`}&#41;</span>
-                        </div>
-                      </div>
-                    </SwiperSlide>
-                  )
-                })}
-            </Swiper>
-          </div>
-          <div className="container shadow-xl/20 max-w-full p-3 my-3 overflow-auto">
-            <p className="text-lg font-sans">&#35; {tag[2]}</p>
-            <Swiper
-              spaceBetween={50}
-              slidesPerView={3}
-              // onSlideChange={() => console.log('slide change')}
-              // onSwiper={swiper => console.log(swiper)}
-            >
-              {kakaoPlace &&
-                kakaoPlace.length > 0 &&
-                kakaoPlace.map((kp, i) => {
-                  if (!kp.category_name.includes(tag[2])) return null
-                  return (
-                    <SwiperSlide key={i}>
-                      <div className="min-w-50">
-                        <img src={Icon} alt="picsum" style={{ minHeight: '200px' }} />
-                        <p className="text-sm">가게명:{kp.place_name}</p>
-                        <div className="text-sm flex gap-3 items-center">
-                          <span>4.3☆</span>
-                          <span>&#40;{`리뷰수`}&#41;</span>
-                        </div>
-                      </div>
-                    </SwiperSlide>
-                  )
-                })}
-            </Swiper>
-          </div>
+          </div> */}
+          {/* 기존 코드 */}
         </>
       )}
     </div>
