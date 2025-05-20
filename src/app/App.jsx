7204d37488135e8374de5dad520d2e95d87769d2
STMT <elahep113@naver.com>
2025-05-20 17:38:58 +0900

    chore: 자잘한 수정들 1. searchresult 경로 변경 2. mypage와 index 위치 변경 3. list 중복 삭제 4. viewplace 화면 재구성

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,30 +1,29 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Mainlayout from '../layouts/Mainlayout.jsx'
 import HomeSearch from '../features/home/HomeSearch.jsx'
 import List from '../features/List.jsx'
-import SearchResult from '../features/SearchResult.jsx'
+import SearchResult from '../features/search/SearchResult.jsx'
 import { useDispatch } from 'react-redux'
 import { authCheck } from '../features/auth/authSlice'
 import LoginPage from '../pages/auth/LoginPage'
-// import ViewPlace from '../features/ViewPlace.jsx'
+import ViewPlace from '../features/search/ViewPlace.jsx'
 import MyPages from '../pages/mypage/MyPage.jsx'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(authCheck())
-  },[dispatch])
+  }, [dispatch])
   return (
     <Routes>
       <Route path="/" element={<Mainlayout />}>
-        <Route path="/mypage" element={<MyPages />}></Route>
         <Route index element={<HomeSearch />}></Route>
-        <Route path='list' element={<List />}></Route>
-        <Route path='search/:query' element={<SearchResult />}></Route>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="mypage" element={<MyPages />}></Route>
         <Route path="list" element={<List />}></Route>
-        {/* <Route path="place/:id" element={<ViewPlace />}></Route> */}
+        <Route path="search/:query" element={<SearchResult />}></Route>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="place/:id" element={<ViewPlace />}></Route>
       </Route>
     </Routes>
   )
