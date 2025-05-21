3b399b245adf086799cd3b4915a87c9b0e8fd887
MunSeungJun <sbs2469@naver.com>
2025-05-21 17:53:35 +0900

    feat: HelpPage 라우팅 설정 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,6 +5,7 @@ import { authCheck } from '../features/auth/authSlice'
 import Mainlayout from '../layouts/Mainlayout'
 import RegistPage from '../pages/auth/RegistPage'
 import LoginPage from '../pages/auth/LoginPage'
+import HelpPage from '../pages/auth/HelpPage'
 
 const App = () => {
   const dispatch = useDispatch()
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/" element={<Mainlayout />}>
         <Route path="regist/:step" element={<RegistPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="help" element={<HelpPage />} />
       </Route>
     </Routes>
   )
