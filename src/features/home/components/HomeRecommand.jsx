b930cea697d99fdbbf57ab1feb250b6cf80b2584
STMT <elahep113@naver.com>
2025-05-21 17:53:14 +0900

    chore: 코드 정리 1. 주석 삭제 2. 상세보기 이동시 state로 데이터를 함께 넘김

--- src/features/home/components/HomeRecommand.jsx
+++ src/features/home/components/HomeRecommand.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import Icon from '../../../assets/images/icon.png'
 import 'swiper/css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import usePlaceStore from '../../../store/usePlaceStore.js'
 import '../../../assets/styles/tailwind.css'
 import '../../../assets/styles/global.css'
@@ -10,8 +10,11 @@ import '../../../assets/styles/global.css'
 const HomeRecommand = () => {
   const [tag, setTag] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-
+  
   const kakaoPlace = usePlaceStore(state => state.kakaoPlace)
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     // console.log(kakaoPlace)
     if (kakaoPlace && kakaoPlace.length > 0) {
@@ -29,6 +32,11 @@ const HomeRecommand = () => {
       setIsLoading(false)
     }
   }, [kakaoPlace])
+
+  const handleNavigate = (kp) => {
+    navigate(`/place/${kp.id}`, {state: kp})
+  }
+
   // console.log(tag)
   // console.log(kakaoPlace)
   return (
@@ -46,8 +54,6 @@ const HomeRecommand = () => {
               <Swiper
                 spaceBetween={50}
                 slidesPerView={3}
-                // onSlideChange={() => console.log('slide change')}
-                // onSwiper={swiper => console.log(swiper)}
               >
                 {kakaoPlace &&
                   kakaoPlace.length > 0 &&
@@ -56,8 +62,8 @@ const HomeRecommand = () => {
                     return (
                       <SwiperSlide key={idx}>
                         <div className="min-w-50">
-                          <img src={Icon} alt="picsum" style={{ minHeight: '200px' }} />
-                          <p className="text-sm">가게명:{kp.place_name}</p>
+                          <img src={Icon} alt="picsum" className='min-h-[200px] mouse_pointer' onClick={() => handleNavigate(kp)} />
+                          <p className="text-sm mouse_pointer" onClick={() => handleNavigate(kp)}>가게명:{kp.place_name}</p>
                           <div className="text-sm flex gap-3 items-center">
                             <span>4.3☆</span>
                             <span>&#40;{`리뷰수`}&#41;</span>
@@ -69,34 +75,6 @@ const HomeRecommand = () => {
               </Swiper>
             </div>
           ))}
-          {/* <div className="container shadow-xl/20 max-w-full p-3 my-3 overflow-auto">
-            <p className="text-lg font-sans">&#35; {tag[0]}</p>
-            <Swiper
-              spaceBetween={50}
-              slidesPerView={3}
-              // onSlideChange={() => console.log('slide change')}
-              // onSwiper={swiper => console.log(swiper)}
-            >
-              {kakaoPlace &&
-                kakaoPlace.length > 0 &&
-                kakaoPlace.map((kp, i) => {
-                  if (!kp.category_name.includes(tag[0])) return null
-                  return (
-                    <SwiperSlide key={i}>
-                      <div className="min-w-50">
-                        <img src={Icon} alt="picsum" style={{ minHeight: '200px' }} />
-                        <p className="text-sm">가게명:{kp.place_name}</p>
-                        <div className="text-sm flex gap-3 items-center">
-                          <span>4.3☆</span>
-                          <span>&#40;{`리뷰수`}&#41;</span>
-                        </div>
-                      </div>
-                    </SwiperSlide>
-                  )
-                })}
-            </Swiper>
-          </div> */}
-          {/* 기존 코드 */}
         </>
       )}
     </div>
