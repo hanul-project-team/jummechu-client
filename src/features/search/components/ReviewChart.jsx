5c822b4cdd5185119658cf11fb1bcc3b12c36866
STMT <elahep113@naver.com>
2025-05-22 18:01:20 +0900

    feat: 리뷰 차트 가게에 대한 리뷰 별점을 차트 형식으로 통계를 알려주는 패키지 사용

---
+++ src/features/search/components/ReviewChart.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, LabelList } from 'recharts'
+
+const ReviewChart = ({ reviews }) => {
+  const rating = reviews.map(rv => {
+    return { rating: rv.rating }
+  })
+  // console.log(rating)
+  const ratingLabel = {
+    5: '매우 만족',
+    4: '만족',
+    3: '보통',
+    2: '불만족',
+    1: '매우불만족',
+  }
+  const countMap = [5, 4, 3, 2, 1].map(rating => {
+    const count = reviews.filter(r => r.rating === rating).length
+    return {
+      rating,
+      name: ratingLabel[rating],
+      count,
+    }
+  })
+  const maxCount = Math.max(...countMap.map(r => r.count))
+  const chartData = countMap.map(r => ({
+    name: r.name,
+    value: Math.round((r.count / maxCount) * 100),
+    count: r.count,
+  }))
+
+  return (
+    <ResponsiveContainer width="100%" height={250}>
+      <BarChart
+        data={chartData}
+        layout="vertical"
+        margin={{ top: 20, right: 30, left: 80, bottom: 5 }}
+      >
+        <XAxis type="number" domain={[0, 'dataMax']} />
+        <YAxis type="category" dataKey="name" />
+        <Tooltip formatter={(value, name, props) => [`${props.payload.count}명`, '응답 수']} />
+        <Bar dataKey="value" fill="cadetblue" barSize={20}>
+          <LabelList dataKey="count" position="right"></LabelList>
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default ReviewChart
