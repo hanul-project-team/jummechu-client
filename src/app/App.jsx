5853a59cf9f1e556650b3798008718c730e8a678
MunSeungJun <sbs2469@naver.com>
2025-05-22 11:18:00 +0900

    refactor: 인증슬라이스 파일 위치 이동

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { authCheck } from '../features/auth/authSlice'
+import { authCheck } from '../features/auth/slice/authSlice'
 import Mainlayout from '../layouts/Mainlayout'
 import RegistPage from '../pages/auth/RegistPage'
 import LoginPage from '../pages/auth/LoginPage'
--- src/layouts/header/MainHeader.jsx
+++ src/layouts/header/MainHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { logout } from '../../features/auth/authSlice'
+import { logout } from '../../features/auth/slice/authSlice'
 import axios from 'axios'
 
 const MainHeader = () => {
