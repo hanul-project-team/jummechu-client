a42e5b26acd4d75996aff6f68af54dca8f77a6e1
STMT <elahep113@naver.com>
2025-05-23 16:01:28 +0900

    chore: list 삭제, searchresult 이름 변경 searchresultpage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,13 +2,12 @@ import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Mainlayout from '../layouts/Mainlayout.jsx'
 import HomeSearch from '../features/home/HomeSearch.jsx'
-import List from '../features/List.jsx'
-import SearchResult from '../features/search/SearchResult.jsx'
+import SearchResultPage from '../pages/place/SearchResultPage.jsx'
 import { useDispatch } from 'react-redux'
 import { authCheck } from '../features/auth/authSlice'
 import RegistPage from '../pages/auth/RegistPage'
 import LoginPage from '../pages/auth/LoginPage'
-import ViewPlace from '../features/search/place/ViewPlace.jsx'
+import ViewPlace from '../pages/place/ViewPlace.jsx'
 import MyPages from '../pages/mypage/MyPage.jsx'
 
 const App = () => {
@@ -21,8 +20,7 @@ const App = () => {
       <Route path="/" element={<Mainlayout />}>
         <Route index element={<HomeSearch />}></Route>
         <Route path="mypage" element={<MyPages />}></Route>
-        <Route path="list" element={<List />}></Route>
-        <Route path="search/:query" element={<SearchResult />}></Route>
+        <Route path="search/:query" element={<SearchResultPage />}></Route>
         <Route path="regist/:step" element={<RegistPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="place/:id" element={<ViewPlace />}></Route>
