4a11e8db4609ebba5b78a55595d6a9821e0e414c
MunSeungJun <sbs2469@naver.com>
2025-05-27 17:03:10 +0900

    feat: 비밀번호 재설정 페이지 토큰 검증 기능 추가

--- src/pages/auth/ResetPasswordPage.jsx
+++ src/pages/auth/ResetPasswordPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 import ResetPasswordForm from '../../features/auth/components/find_account/ResetPasswordForm'
 
 const ResetPasswordPage = () => {
+  const navigate = useNavigate()
+  const [searchParams, _setSearchParams] = useSearchParams()
+  const resetToken = searchParams.get('token')
+  useEffect(() => {
+    if (resetToken) {
+      const verifyResetToken = async () => {
+        try {
+          await axios.post('http://localhost:3000/auth/verify_reset_token', {
+            resetToken,
+          })
+        } catch {
+          alert('유효하지 않거나 만료된 링크입니다.')
+          navigate('/find_account?type=password')
+        }
+      }
+      verifyResetToken()
+    }
+  }, [resetToken, navigate])
+
   return (
     <main className="container mx-auto flex justify-center ">
       <section className={`max-w-sm w-full flex flex-col gap-5`}>
@@ -19,15 +41,15 @@ const ResetPasswordPage = () => {
                 fill="#75DBFF"
               ></path>
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M15.75 17.5001C11.884 17.5001 8.75 20.6341 8.75 24.5001V43.6528C8.75 47.5188 11.884 50.6528 15.75 50.6528H40.25C44.116 50.6528 47.25 47.5188 47.25 43.6528V24.5001C47.25 20.6341 44.116 17.5001 40.25 17.5001H15.75ZM30.1393 37.3746C32.0278 36.5495 33.3476 34.6651 33.3476 32.4723C33.3476 29.5191 30.9535 27.1251 28.0003 27.1251C25.0472 27.1251 22.6532 29.5191 22.6532 32.4723C22.6532 34.6651 23.9729 36.5495 25.8615 37.3746V39.9583C25.8615 41.1396 26.8191 42.0972 28.0003 42.0972C29.1816 42.0972 30.1393 41.1396 30.1393 39.9583V37.3746Z"
                 fill="#00B9FF"
               ></path>
             </svg>
             <h2 className="font-bold text-xl">비밀번호를 재설정해 주세요</h2>
           </div>
-          <ResetPasswordForm />
+          <ResetPasswordForm resetToken={resetToken} />
         </div>
       </section>
     </main>
