329c89ade924ca0583ec94c848e2d5d0880eec2c
MunSeungJun <sbs2469@naver.com>
2025-05-27 17:04:30 +0900

    feat: 비밀번호 재설정 링크 발송 요청 추가

--- src/features/auth/components/find_account/AccountFound.jsx
+++ src/features/auth/components/find_account/AccountFound.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { reset } from '../../slice/findAccountSlice'
+import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import style from './accountFound.module.css'
 
 const AccountFound = ({ type }) => {
+  const [isRequested, setIsRequested] = useState(false)
   const user = useSelector(state => state.findAccount.user)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    return () => {
-      dispatch(reset())
+  const sendLink = async () => {
+    try {
+      await axios.post('http://localhost:3000/auth/send_link', user)
+      setIsRequested(true)
+    } catch {
+      toast.error(
+        <div>
+          서버 오류가 발생했습니다.
+          <br />
+          잠시 후 다시 시도해주세요.
+        </div>,
+        { autoClose: 3000 },
+      )
     }
-  }, [dispatch])
+  }
   return (
     <>
       <div className="flex flex-col gap-1 w-full items-center py-3 bg-gray-200 rounded-sm">
@@ -33,12 +45,9 @@ const AccountFound = ({ type }) => {
         )}
         {type === 'password' && (
           <>
-            <button className="grow border rounded-sm px-2 py-3 text-center text-white bg-black">
-              재전송 하기
+            <button onClick={sendLink} disabled={isRequested} className={`${style.sendBtn}`}>
+              재설정 링크 보내기
             </button>
-            <Link to="/login" className="grow border rounded-sm px-2 py-3 text-center ">
-              로그인 하기
-            </Link>
           </>
         )}
       </div>
