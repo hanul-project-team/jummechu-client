dec0bb7986f4241a529b31d6a51d6b7c50eea840
STMT <elahep113@naver.com>
2025-05-27 09:14:49 +0900

    chore: 주석 삭제

--- src/pages/place/ViewPlace.jsx
+++ src/pages/place/ViewPlace.jsx
@@ -4,14 +4,12 @@ import ViewPlaceDetail from '../../features/place/ViewPlaceDetail.jsx'
 import { useLocation } from 'react-router-dom'
 
 const ViewPlace = () => {
-  // const [placeInfo, setPlaceInfo] = useState([])
   const [defaultBookmarked, setdefaultBookmarked] = useState(false)
   const location = useLocation()
   const setPlaceDetail = zustandStore(state => state.setPlaceDetail)
-
+  
   useEffect(() => {
     if (location.state) {
-      // setPlaceInfo(location.state)
       setPlaceDetail(location.state)
     }
   }, [location.state, defaultBookmarked])
