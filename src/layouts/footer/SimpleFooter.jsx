f782b0da7e3ba28d18fc7bfca02d4249dea5e4e6
MunSeungJun <sbs2469@naver.com>
2025-05-29 17:56:39 +0900

    chore: simpleFooter container max-w 수정

--- src/layouts/footer/SimpleFooter.jsx
+++ src/layouts/footer/SimpleFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SimpleFooter = () => {
   return (
     <footer className="bg-color-gray-50 mt-16 pt-8 pb-10">
-      <div className="container mx-auto px-6">
+      <div className="container max-w-5xl mx-auto px-6">
         <div className="text-color-gray-700 flex justify-center">
           <span>&copy; 2025 Jummechu All Right Reserved</span>
         </div>
