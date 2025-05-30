add96c302ef53ae29772fee07b9f9159d9eedb9b
MunSeungJun <sbs2469@naver.com>
2025-05-30 17:56:23 +0900

    chore: loginSchema 비밀번호 에러 메시지 수정 및 required_error 메시지 설정

--- src/features/auth/schema/loginSchema.js
+++ src/features/auth/schema/loginSchema.js
@@ -1,7 +1,16 @@
 import z from 'zod'
 
 export const loginSchema = z.object({
-  email: z.string().trim().email('올바른 이메일 형식을 입력해주세요.'),
-  password: z.string().trim().regex(/^(?=.*[A-Z,a-z])(?=.*\d)(?=.*[~!@#^*_=+-]).{8,}$/, '이메일 또는 비밀번호가 잘못 되었습니다. 이메일과 비밀번호를 정확히 입력해 주세요'),
-  rememberMe: z.boolean().optional()
+  email: z
+    .string({ required_error: '올바른 이메일 형식을 입력해주세요' })
+    .trim()
+    .email('올바른 이메일 형식을 입력해주세요'),
+  password: z
+    .string({ required_error: '영문, 숫자, 특수문자(~!@#^*_=+-) 포함 8자 이상이어야 합니다' })
+    .trim()
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#^*_=+-]).{8,}$/,
+      '영문, 숫자, 특수문자(~!@#^*_=+-) 포함 8자 이상이어야 합니다',
+    ),
+  rememberMe: z.boolean().optional(),
 })
