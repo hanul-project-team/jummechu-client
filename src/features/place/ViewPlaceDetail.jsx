f3f97ab84f10a526951bb4b8ab56ebad70be8a80
STMT <elahep113@naver.com>
2025-05-30 12:37:36 +0900

    chore: 주석 삭제

--- src/features/place/ViewPlaceDetail.jsx
+++ src/features/place/ViewPlaceDetail.jsx
@@ -186,7 +186,6 @@ const ViewPlaceDetail = () => {
               d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
             />
           </svg>
-          {/* 지도pin 아이콘 */}
           <p>{placeDetail.address}</p>
         </div>
         {/* 전화 */}
