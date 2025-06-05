9be42e1de7e9c93c83870b58a9bf1ae3904f3ec8
STMT <elahep113@naver.com>
2025-06-05 12:04:26 +0900

    chore: 검색결과 페이지 component 분리

---
+++ src/features/search/SearchResultPageFilter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchResultPageFilter = ({ search }) => {
+  console.log(search)
+  return (
+    <>
+      <p className="text-end pointer-events-none select-none">필터</p>
+      <div className="border-1 h-7">
+        <p>무슨 필터를 넣어야하지</p>
+      </div>
+    </>
+  )
+}
+
+export default SearchResultPageFilter
---
+++ src/features/search/SearchResultPageList.jsx
@@ -0,0 +1,75 @@
+import Rating from 'react-rating'
+import StarYellow from '../../assets/images/star-yellow.png'
+
+const SearchResultPageList = ({ search, navi, avg, npr, count, extract, tag, filter }) => {
+  return (
+    <>
+      <span className="ml-3 pointer-events-none select-none">{search.length} 개의 검색 결과</span>
+      {search.map((sd, i) => {
+        return (
+          <div key={i} className="flex gap-2 p-2 my-3">
+            <div className="md:min-w-[200px]">
+              <img
+                src={`https://picsum.photos/200/200?random=${Math.floor(Math.random() * 1000)}`}
+                alt="picsum"
+                className="md:w-[200px] md:h-[200px] sm:w-[150px] sm:h-[150px] hover:cursor-pointer object-cover rounded-lg"
+                onClick={() => navi(sd)}
+              />
+            </div>
+            <div className="md:max-h-[200px] overflow-y-auto">
+              <span className="hover:cursor-pointer text-2xl" onClick={() => navi(sd)}>
+                <strong>{sd.place_name}</strong>
+              </span>
+              <p>
+                <strong>주소지</strong>:{sd.address_name}
+              </p>
+              <p>
+                {sd.phone ? (
+                  <>
+                    <strong>연락처: </strong>
+                    <span>{sd.phone}</span>
+                  </>
+                ) : (
+                  '연락처 미공개'
+                )}
+              </p>
+              <div className="flex items-center">
+                <p>
+                  <strong>사용자 평점</strong>:{' '}
+                </p>
+                <span className="flex">
+                  <Rating
+                    initialRating={1}
+                    fullSymbol={<img src={StarYellow} className="w-4 h-4" />}
+                    emptySymbol={<img src={StarYellow} className="w-4 h-4" />}
+                    stop={1}
+                    readonly
+                  />
+                  <p>{avg(npr, sd)}</p>&nbsp;
+                </span>
+                <span>&#40;{count(npr, sd)}&#41;</span>
+              </div>
+              <div className="flex gap-1 py-1">
+                {tag?.length > 0 && (
+                  <p className="border-1 rounded-2xl px-2 py-1 text-white bg-color-teal-400">
+                    {tag.filter(tg => tg === extract(sd))}
+                  </p>
+                )}
+                {filter(sd).map((key, i) => (
+                  <p
+                    key={i}
+                    className={`${key ? 'border-1 rounded-2xl px-2 py-1 border-gray-700 bg-color-gray-50' : null}`}
+                  >
+                    {key}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default SearchResultPageList
