e34c49fd81728af2ba472aab64a0a1595b4b5d7d
STMT <elahep113@naver.com>
2025-06-13 17:58:49 +0900

    chore: 상세 페이지 주변장소 저장 및 로딩 설정

--- src/features/place/components/RecommandPlace.jsx
+++ src/features/place/components/RecommandPlace.jsx
@@ -7,12 +7,10 @@ import { toast } from 'react-toastify'
 import zustandStore from '../../../app/zustandStore.js'
 import { useNavigate } from 'react-router-dom'
 
-const RecommandPlace = ({ placeDetail }) => {
-  const setIsLoading = zustandStore(state => state.setIsLoading)
-  const userNearPlace = zustandStore(state => state.userNearPlace)
+const RecommandPlace = ({ placeDetail, setLoading, loading }) => {
   const searchNearData = zustandStore(state => state.searchNearData)
   const navigate = useNavigate()
-  
+
   const handleNavigate = snd => {
     if (snd) {
       axios
@@ -20,16 +18,14 @@ const RecommandPlace = ({ placeDetail }) => {
         .then(res => {
           const data = res.data
           if (data !== null && data._id) {
-            console.log('1-2 데이터 있음')
-            console.log(data)
-            // console.log(data._id)
+            // console.log('1-2 데이터 있음')
             try {
               navigate(`/place/${data._id}`, { state: data })
             } catch (err) {
               console.error(`data 아이디 에러 ${data._id}`)
             }
           } else if (data === null) {
-            setIsLoading(true)
+            setLoading(true)
             window.scrollTo({ top: 0 })
             console.log('2-1 데이터 없음, 등록 실행')
             axios
@@ -39,8 +35,10 @@ const RecommandPlace = ({ placeDetail }) => {
                 // console.log(place)
                 if (Array.isArray(place)) {
                   navigate(`/place/${place[0]._id}`, { state: place[0] })
+                  setLoading(false)
                 } else {
                   navigate(`/place/${place._id}`, { state: place })
+                  setLoading(false)
                 }
               })
               .catch(err => {
@@ -66,34 +64,60 @@ const RecommandPlace = ({ placeDetail }) => {
 
   return (
     <>
-      {searchNearData.filter(snd => snd.id !== placeDetail.id).length > 0 && (
-        <div className="max-w-full my-5 min-h-[200px]">
-          <div className="text-start">
-            <p className="font-bold text-lg">다른 장소도 둘러보세요!</p>
+      {loading === true ? (
+        <div>
+          <div className="container max-w-full mx-auto text-center sm:min-h-[200px]">
+            <p className="loading-jump">
+              Loading
+              <span className="jump-dots">
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+              </span>
+            </p>
           </div>
-          {searchNearData && (
-            <Swiper spaceBetween={50} slidesPerView={3} className="border-t-1 border-gray-700 flex">
-              {searchNearData
-                // .filter(snd => snd.id !== placeDetail._id)
-                .map((snd, i) => {
-                  return (
-                    <SwiperSlide key={i} className="gap-3 p-2 sm:max-w-[14rem] text-center !mr-0">
-                      <div key={i}>
-                        <div className="hover:cursor-pointer" onClick={() => handleNavigate(snd)}>
-                          <img src={Icon} alt="icon" className="w-[100px] h-[100px] mx-auto" />
-                        </div>
-                        <div>
-                          <p className="hover:cursor-pointer" onClick={() => handleNavigate(snd)}>
-                            {snd.place_name}
-                          </p>
-                        </div>
-                      </div>
-                    </SwiperSlide>
-                  )
-                })}
-            </Swiper>
-          )}
         </div>
+      ) : (
+        searchNearData?.length > 0 && (
+          <div className="max-w-full my-5 min-h-[200px]">
+            <div className="text-start">
+              <p className="font-bold text-lg">다른 장소도 둘러보세요!</p>
+            </div>
+            {searchNearData && (
+              <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                className="border-t-1 border-gray-700 flex"
+              >
+                {searchNearData
+                  .filter(snd => snd._id !== placeDetail._id)
+                  .map((snd, i) => {
+                    return (
+                      <SwiperSlide key={i} className="gap-3 p-2 sm:max-w-[14rem] text-center !mr-0">
+                        <div key={i}>
+                          <div className="hover:cursor-pointer" onClick={() => handleNavigate(snd)}>
+                            <img
+                              src={snd.photos[0] ? snd.photos[0] : Icon}
+                              alt="icon"
+                              className="w-[150px] h-[150px] mx-auto rounded-xl"
+                            />
+                          </div>
+                          <div>
+                            <p
+                              className="hover:cursor-pointer font-bold"
+                              onClick={() => handleNavigate(snd)}
+                            >
+                              {snd.place_name ? snd.place_name : snd.name}
+                            </p>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                    )
+                  })}
+              </Swiper>
+            )}
+          </div>
+        )
       )}
     </>
   )
