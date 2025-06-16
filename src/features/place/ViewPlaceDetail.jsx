e62d66ed0b1ff08ea610ee4a4d9a1450f747f9f1
STMT <elahep113@naver.com>
2025-06-16 16:32:46 +0900

    feat: 상세페이지 지도 및 마커띄우기 완료

--- src/features/place/ViewPlaceDetail.jsx
+++ src/features/place/ViewPlaceDetail.jsx
@@ -19,7 +19,6 @@ const ViewPlaceDetail = () => {
   const setPlaceDetail = zustandStore(state => state.setPlaceDetail)
   const navigate = useNavigate()
   const setSearchNearData = zustandStore(state => state.setSearchNearData)
-  const searchNearData = zustandStore(state => state.searchNearData)
   const lastStoreRef = useRef(placeDetail?._id)
   const lastReviewRef = useRef(reviewInfo)
   const user = useSelector(state => state.auth.user)
@@ -30,6 +29,15 @@ const ViewPlaceDetail = () => {
   const rootLocation = `${window.location.origin}`
   const location = useLocation()
   const [recommandLoading, setRecommandLoading] = useState(false)
+  const [center, setCenter] = useState({
+    lat: 37.3946622,
+    lng: 127.1026676,
+  })
+  const [map, setMap] = useState(null)
+  const [marker, setMarker] = useState({
+    lat: 37.3946622,
+    lng: 127.1026676,
+  })
 
   // console.log(placeDetail.keywords)
   /* 정보 호출 및 갱신 */
@@ -107,7 +115,71 @@ const ViewPlaceDetail = () => {
         })
     }
   }, [location.pathname, placeDetail, setPlaceDetail])
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_KEY}&autoload=false`
+    script.async = true
+    document.head.appendChild(script)
+    let isMounted = true
 
+    script.onload = () => {
+      if (!isMounted) return
+      window.kakao.maps.load(() => {
+        if (!isMounted) return
+        const container = document.getElementById('map')
+        if (container) {
+          const options = {
+            center: new window.kakao.maps.LatLng(center.lat, center.lng),
+            level: 3,
+          }
+          const kakaoMap = new window.kakao.maps.Map(container, options)
+          setMap(kakaoMap)
+          const initialMarkerPosition = new window.kakao.maps.LatLng(center.lat, center.lng)
+          const newMarker = new window.kakao.maps.Marker({
+            position: initialMarkerPosition,
+            map: kakaoMap,
+          })
+          setMarker(newMarker)
+        } else {
+          console.error('Error: Map container #map not found!')
+        }
+      })
+    }
+
+    return () => {
+      isMounted = false
+      document.head.removeChild(script)
+      if (marker && typeof marker === 'function') {
+        marker?.setMap(null)
+      } else {
+        console.warn('마커 삭제. setMap이 유효하지 않음.')
+      }
+    }
+  }, [])
+  useEffect(() => {
+    if (placeDetail) {
+      setCenter({
+        lat: placeDetail?.latitude,
+        lng: placeDetail?.longitude,
+      })
+    }
+  }, [placeDetail])
+  useEffect(() => {
+    if (map && center) {
+      const moveLatLon = new window.kakao.maps.LatLng(center.lat, center.lng)
+      map.setCenter(moveLatLon)
+      if (marker) {
+        marker.setPosition(moveLatLon)
+      } else {
+        const newMarker = new window.kakao.map.Marker({
+          position: moveLatLon,
+          map: map,
+        })
+        setMarker(newMarker)
+      }
+    }
+  }, [map, center, marker])
+  // console.log(center.lat, center.lng)
   const handleBookmark = () => {
     if (!user.role) {
       if (confirm('로그인이 필요한 기능입니다. 로그인 하시겠습니까?')) {
@@ -267,72 +339,80 @@ const ViewPlaceDetail = () => {
                 className="w-full h-[300px] rounded-xl"
               />
             </div>
-            {/* 주소지 */}
-            <div className="flex gap-2 relative my-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-                />
-              </svg>
-              <p>{placeDetail.address}</p>
-            </div>
-            {/* 전화 */}
-            <div className="flex gap-2 my-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
-                />
-              </svg>
-              <p>{placeDetail?.phone ? placeDetail.phone : '연락처 미제공'}</p>
-            </div>
-            {/* 문의? */}
-            <div className="flex gap-2 my-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-                />
-              </svg>
-              <div className="flex gap-2">
-                <Link>
-                  <p>폐업 신고</p>
-                </Link>
-                <span>&#183;</span>
-                <Link>
-                  <p>정보수정 제안</p>
-                </Link>
+            {/* 가게 정보란 */}
+            <div className="flex items-start justify-between">
+              {/* 상세정보 */}
+              <div>
+                {/* 주소지 */}
+                <div className="flex gap-2 relative my-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                    />
+                  </svg>
+                  <p>{placeDetail.address}</p>
+                </div>
+                {/* 전화 */}
+                <div className="flex gap-2 my-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                    />
+                  </svg>
+                  <p>{placeDetail?.phone ? placeDetail.phone : '연락처 미제공'}</p>
+                </div>
+                {/* 문의? */}
+                <div className="flex gap-2 my-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                  </svg>
+                  <div className="flex gap-2">
+                    <Link>
+                      <p>폐업 신고</p>
+                    </Link>
+                    <span>&#183;</span>
+                    <Link>
+                      <p>정보수정 제안</p>
+                    </Link>
+                  </div>
+                </div>
               </div>
+              {/* 지도 */}
+              <div id="map" style={{ width: '500px', height: '400px' }}></div>
             </div>
             {/* 다른 장소 추천 */}
             <RecommandPlace
