06e484aad6b7b89eb31a50d949464d118297040a
seungjun <sbs2469@naver.com>
2025-06-17 00:08:15 +0900

    chore: 이용약관 필드 유효성 검증 조건 추가

--- src/features/auth/schema/registSchema.js
+++ src/features/auth/schema/registSchema.js
@@ -12,8 +12,11 @@ export const registScheam = z
       .string()
       .trim()
       .regex(/^[가-힣]{2,5}$/, '이름은 두 글자 이상 입력해주세요.'),
-    phone: z.string().regex(/^01[016789][0-9]{8}$/,'휴대전화 번호를 확인해주세요'),
-    code: z.string().regex(/^\d{6}$/)
+    phone: z.string().regex(/^01[016789][0-9]{8}$/, '휴대전화 번호를 확인해주세요'),
+    code: z.string().regex(/^\d{6}$/),
+    service: z.boolean(),
+    privacy: z.boolean(),
+    business: z.boolean(),
   })
   .refine(data => data.password === data.passwordCheck, {
     path: ['passwordCheck'],
