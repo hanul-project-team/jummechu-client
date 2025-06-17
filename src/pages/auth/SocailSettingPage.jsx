6acd0b71bd9fe2b2a3bd3f71f7364fafd68291c0
MunSeungJun <sbs2469@naver.com>
2025-06-17 16:39:52 +0900

    feat: route state 전달 및 받기 로직 추가

--- src/pages/auth/SocailSettingPage.jsx
+++ src/pages/auth/SocailSettingPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import AccountSettingForm from '../../features/auth/components/social/AccountSettingForm'
 
 const SocailSettingPage = () => {
+  const location = useLocation()
+  const returnUrl = location.state?.returnUrl
   return (
     <main className="container mx-auto max-w-5xl flex justify-center px-6 ">
       <section className="max-w-sm w-full flex flex-col">
@@ -9,7 +12,7 @@ const SocailSettingPage = () => {
           <h2 className="text-center text-2xl font-semibold cursor-default">
             계정 설정을 완료해주세요
           </h2>
-          <AccountSettingForm />
+          <AccountSettingForm returnUrl={returnUrl} />
         </div>
       </section>
     </main>
