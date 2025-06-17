c77ce3236b52063a1493f9fd07e107794a0ff563
STMT <elahep113@naver.com>
2025-06-17 12:40:49 +0900

    chore: 아이콘 디폴트 이미지 변경 및 홈 표출 가게 정보 수정

--- src/features/home/HomeRecommand.jsx
+++ src/features/home/HomeRecommand.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import Icon from '../../assets/images/icon.png'
+import Icon from '../../assets/images/default2.png'
 import { API } from '../../app/api.js'
 import 'swiper/css'
 import { toast } from 'react-toastify'
@@ -305,37 +305,41 @@ const HomeRecommand = () => {
                             className="sm:h-[200px] hover:cursor-pointer rounded-xl"
                             onClick={() => handleNavigate(fps)}
                           />
-                          <p
-                            className="text-sm hover:cursor-pointer"
-                            onClick={() => handleNavigate(fps)}
-                          >
-                            가게명: <strong>{fps.name}</strong>
-                          </p>
-                          <div className="text-sm flex gap-1 items-center">
-                            {
-                              <div className="flex items-center cursor-default">
-                                {handleAvgRating(nearPlaceReviews, fps)}
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="size-4"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-                                  />
-                                </svg>
-                              </div>
-                            }
-                            <span className="cursor-default">
-                              리뷰수&#40;{handleCountReviews(nearPlaceReviews, fps)}&#41;
-                            </span>
-                            <span className="cursor-default">{fps.address}</span>
+                          <div className="flex flex-col">
+                            <p className="text-md">
+                              <strong
+                                onClick={() => handleNavigate(fps)}
+                                className="hover:cursor-pointer"
+                              >
+                                {fps.name}
+                              </strong>
+                            </p>
+                            <div className="text-sm flex gap-1 items-center">
+                              {
+                                <div className="flex items-center cursor-default">
+                                  {handleAvgRating(nearPlaceReviews, fps)}
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-4"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                                    />
+                                  </svg>
+                                </div>
+                              }
+                              <span className="cursor-default">
+                                리뷰수&#40;{handleCountReviews(nearPlaceReviews, fps)}&#41;
+                              </span>
+                            </div>
                           </div>
+                          <span className="cursor-default text-sm">{fps.address}</span>
                         </SwiperSlide>
                       )
                     })}
