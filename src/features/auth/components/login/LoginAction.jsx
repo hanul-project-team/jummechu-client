4e8314f14b4638a7d7445d8db5774259545776e0
seungjun <sbs2469@naver.com>
2025-06-21 20:22:41 +0900

    feat: loginAction 구글간편 로그인 버튼 추가

--- src/features/auth/components/login/LoginAction.jsx
+++ src/features/auth/components/login/LoginAction.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { login } from '../../slice/authSlice'
+import { API } from '../../../../app/api'
+import { toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
+import GoogleButton from '../../../../shared/GoogleButton'
 
 const LoginAction = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const onLogin = async response => {
+    const idToken = response.credential
+    try {
+      const response = await API.post('/auth/google_verify', { token: idToken })
+      dispatch(login(response.data))
+      navigate('/social_setting')
+    } catch {
+      toast.error(
+        <div className="Toastify__toast-body cursor-default">잠시 후 다시 시도해주세요</div>,
+        {
+          position: 'top-center',
+        },
+      )
+    }
+  }
   return (
-    <div className="flex justify-center items-center gap-2 text-base ">
-      <span className="text-color-gray-700 cursor-default">아직 점메추 회원이 아니신가요?</span>
-      <Link to="/regist/method" className="text-color-teal-400 font-semibold hover:underline outline-hidden">
-        회원가입
-      </Link>
-    </div>
+    <>
+      <div className="flex items-center justify-center gap-2">
+        <span className="bg-color-gray-300 h-0.25 grow-1"></span>
+        <span className="grow-0">또는</span>
+        <span className="bg-color-gray-300 h-0.25 grow-1"></span>
+      </div>
+      <GoogleButton
+        className="p-3 gap-2 font-semibold border border-color-gray-300 rounded-lg"
+        value="Google로 로그인"
+        onLogin={onLogin}
+      />
+      <div className="flex justify-center items-center gap-2 text-base ">
+        <span className="text-color-gray-700 cursor-default">아직 점메추 회원이 아니신가요?</span>
+        <Link
+          to="/regist/method"
+          className="text-color-teal-400 font-semibold hover:underline outline-hidden"
+        >
+          회원가입
+        </Link>
+      </div>
+    </>
   )
 }
 
