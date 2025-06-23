10a7f52a3e329bc1ee34d454b467cdb949b34c73
STMT <elahep113@naver.com>
2025-06-23 14:14:14 +0900

    chore: 이미지 src 적용방식 변경

--- src/features/place/ViewPlaceDetail.jsx
+++ src/features/place/ViewPlaceDetail.jsx
@@ -345,7 +345,7 @@ const ViewPlaceDetail = () => {
               </div>
               <div className="min-sm:row-span-2 px-2">
                 <img
-                  src={`${import.meta.env.VITE_API_BASE_URL + placeDetail?.photos?.[0]}` || Icon}
+                  src={`${placeDetail?.photos?.[0] ? import.meta.env.VITE_API_BASE_URL + placeDetail?.photos?.[0] : Icon}`}
                   alt={`${placeDetail?.photos?.[0] ? 'photos' : 'Icon'}`}
                   className="sm:h-auto w-fit rounded-xl max-sm:mx-auto"
                   onError={e => {
