97aa66afdd76ca41069c63a6ea9f4858d988eabb
STMT <elahep113@naver.com>
2025-06-23 10:56:10 +0900

    chore: location.state가 없을때 params.id를 통해 장소 정보를 받아오도록 변경

--- src/pages/place/ViewPlace.jsx
+++ src/pages/place/ViewPlace.jsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import zustandStore from '../../app/zustandStore.js'
 import ViewPlaceDetail from '../../features/place/ViewPlaceDetail.jsx'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
+import { API } from '../../app/api.js'
 
 const ViewPlace = () => {
   const location = useLocation()
   const setPlaceDetail = zustandStore(state => state.setPlaceDetail)
   const setIsLoading = zustandStore(state => state.setIsLoading)
-  
+  const params = useParams()
+  const placeId = params.id
   useEffect(() => {
-    if (location.state) {
+    if (location.state && location.state._id === placeId) {
       setPlaceDetail(location.state)
-      if(location.state?._id) {
-        setIsLoading(false)
-      }
+      setIsLoading(false)
+    } else {
+      API.get(`/store/read/${placeId}`)
+        .then(res => {
+          setPlaceDetail(res.data)
+          setIsLoading(false)
+        })
+        .catch(err => {
+          console.error(err)
+          setIsLoading(false)
+        })
     }
-  }, [location.state])
+  }, [location.state, placeId])
 
   useEffect(() => {
     window.scrollTo({ top: 0 })
