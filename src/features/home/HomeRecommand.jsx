61b13501ddea4981cfbd8551c50f503295b35701
STMT <elahep113@naver.com>
2025-06-25 10:36:50 +0900

    chore: 홈 화면 별 이미지 변경

--- src/features/home/HomeRecommand.jsx
+++ src/features/home/HomeRecommand.jsx
@@ -6,6 +6,7 @@ import 'swiper/css'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
 import zustandStore from '../../app/zustandStore.js'
+import StarYellow from '../../assets/images/star-yellow.png'
 
 const HomeRecommand = () => {
   const [isLoading, setIsLoading] = useState(true)
@@ -329,21 +330,8 @@ const HomeRecommand = () => {
                             <div className="sm:flex sm:flex-justify gap-1 items-center">
                               {
                                 <div className="flex items-center cursor-default">
-                                  {handleAvgRating(nearPlaceReviews, fps)}
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="size-4"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-                                    />
-                                  </svg>
+                                  <p>{handleAvgRating(nearPlaceReviews, fps)}</p>
+                                  <img src={StarYellow} alt="star-yellow" className='w-4 h-4 translate-y-[-1px]' />
                                 </div>
                               }
                               <span className="cursor-default min-sm:text-md max-sm:text-sm">
